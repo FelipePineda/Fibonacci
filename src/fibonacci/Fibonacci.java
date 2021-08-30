071eed459aa7b8800668b92423c72a6701bbfff7
Felipe Pineda <felipe.3801911628@ucaldas.edu.co>
2021-08-29 22:21:01 -0500

    Metodo para saber si el numero es primo o no

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -16,11 +16,12 @@ public class Fibonacci {
      */
     public static void main(String[] args) {
         
-                int fibo = 10, num1 = 0, num2 = 1, suma = 1;
+                int numero = 7, num1 = 0, num2 = 1, suma = 1;
 
-        System.out.println(num1);
-         
-        for (int i = 1; i < fibo; i++) {
+        System.out.println("El numero es primo? "+esPrimo(numero));
+         System.out.println(num1);
+        
+        for (int i = 1; i < numero; i++) {
          
             System.out.println(suma);
          
@@ -33,5 +34,16 @@ public class Fibonacci {
         }
         
     }
+      
     
+    public static boolean esPrimo(int numero){
+  int contador = 2;
+  boolean primo=true;
+  while ((primo) && (contador!=numero)){
+    if (numero % contador == 0)
+      primo = false;
+    contador++;
+  }
+  return primo;
+}
 }
